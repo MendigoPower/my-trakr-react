e6d8d95ab1b133dea2e3146151e830a850d63437
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-07-26 13:37:21 -0700

    simple updates

--- src/components/AccountSummary.jsx
+++ src/components/AccountSummary.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react';
+import axios from 'axios';
 
-export default function AccountSummary() {
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { amount }
+  }
+}
   return (
-    <div>
+    <div className='App'>
+      <p>{amount}</p>
       
-    </div>
+    </div> 
   )
 }
