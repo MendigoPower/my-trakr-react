8148e5341d3037bbd59fec81ce0a498c624b0888
Gabriel Soares de Brito <86431420+MendigoPower@users.noreply.github.com>
2022-07-26 16:24:20 -0700

    styled components added

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 export default function Account(props) {
   // const accounts = props.accounts;
